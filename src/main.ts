1724dcedcdb548937894f2bce30605173f60f6ab
levg34 <levg34@users.noreply.github.com>
2022-10-13 22:31:41 +0200

    extract Playground class to separate file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 import * as BABYLON from 'babylonjs'
+import { Playground } from './playground'
 
 const canvas = document.getElementById('renderCanvas')
 
@@ -17,30 +18,6 @@ const startRenderLoop = function (engine: BABYLON.Engine, canvas: HTMLElement | 
 
 const createDefaultEngine = function () {
     return new BABYLON.Engine(canvas as BABYLON.Nullable<HTMLCanvasElement>, true, { preserveDrawingBuffer: true, stencil: true, disableWebGL2Support: false })
-}
-
-class Playground {
-    static CreateScene(engine: BABYLON.Engine, canvas: BABYLON.Nullable<HTMLCanvasElement>) {
-        // This creates a basic Babylon Scene object (non-mesh)
-        const scene = new BABYLON.Scene(engine)
-        // This creates and positions a free camera (non-mesh)
-        const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene)
-        // This targets the camera to scene origin
-        camera.setTarget(BABYLON.Vector3.Zero())
-        // This attaches the camera to the canvas
-        camera.attachControl(canvas, true)
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene)
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7
-        // Our built-in 'sphere' shape. Params: name, options, scene
-        const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2, segments: 32 }, scene)
-        // Move the sphere upward 1/2 its height
-        sphere.position.y = 1
-        // Our built-in 'ground' shape. Params: name, options, scene
-        const ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, scene)
-        return scene
-    }
 }
 
 const createScene = function () {
